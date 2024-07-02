cf0cbc9c25dc47034a793b0a02f8275ae0861280
Abishek-GH <abishek0149@gmail.com>
2024-07-03 01:26:30 +0530

    Edit Bio

--- script.js
+++ script.js
@@ -46,15 +46,15 @@ ScrollReveal({
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Associate Software Engineer', 'Backend Developer'],
+    strings: ['Associate Software Engineer', 'Software Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
