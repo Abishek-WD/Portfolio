4d2cc6e78eaa85cfe526219bada7a4e3904288be
Abishek-GH <abishek0149@gmail.com>
2024-10-12 16:48:52 +0530

    Update Span

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Associate Software Engineer', 'Software Developer'],
+    strings: ['Associate Software Engineer', 'Backend Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
